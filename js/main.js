71859ef6b3e92ebde44a2b3a414fed57e4da7f03
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-07-02 13:38:08 +0200

    bootstrap - warsztaty 5

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(window).scroll(function() {
     if (scroll >= 50) {
         $("#main-nav").css('background-color','#0ed8d6').css('opacity','0.9')
     }else{
-        $("#main-nav").css('background-color','transparent').css('opacity','1');
+        $("#main-nav").css('background-color','transparent').css('opacity','1')
     }
 });
                  
