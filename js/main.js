1a7514820b2e8b06f2f8d3921f84248b2eb60194
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-07-02 12:13:44 +0200

    bootstrap - warsztaty 4

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
-$("fadeout").(function(){
-    $("header").fadeOut();
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();    
+    if (scroll >= 50) {
+        $("#main-nav").css('background-color','#0ed8d6').css('opacity','0.9')
+    }else{
+        $("#main-nav").css('background-color','transparent').css('opacity','1');
+    }
 });
+                 
